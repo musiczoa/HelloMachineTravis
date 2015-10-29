f9adaea56f4ed16c5183585da043db91fdebc75c
Hwa Hyeon <hwa.hyeon@insertcoin.me>
2015-10-29 20:24:33 +0900

    package 이름 수정

--- app/src/main/java/me/insertcoin/hellomachine/MainActivity.java
+++ app/src/main/java/me/insertcoin/hellomachine/MainActivity.java
@@ -1,4 +1,4 @@
-package insertcoin.me.hellomachine;
+package me.insertcoin.hellomachine;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import insertcoin.me.hellomachine.R;
 
 public class MainActivity extends AppCompatActivity {
 
