812b99b1ccb5d391282be803a021dc76216cc867
Hwa Hyeon <hwa.hyeon@insertcoin.me>
2015-10-29 20:41:45 +0900

    Java에서 Native 코드 호출 성공! 짝짝짝

--- app/src/main/java/me/insertcoin/hellomachine/MainActivity.java
+++ app/src/main/java/me/insertcoin/hellomachine/MainActivity.java
@@ -2,8 +2,10 @@ package me.insertcoin.hellomachine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import insertcoin.me.hellomachine.R;
+import me.insertcoin.hellomachine.swig.Greeter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Greeter greeter = new Greeter();
+        ((TextView)findViewById(R.id.textView)).setText(greeter.Greet("안녕?"));
     }
 }
